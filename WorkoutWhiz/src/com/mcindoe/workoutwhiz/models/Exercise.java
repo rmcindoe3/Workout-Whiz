c4cfccc7f6c09f35de0af510d80f67290b60a3d6
rmcindoe3 <rmcindoe3@gatech.edu>
2014-02-26 18:13:40 -0500

    Exercise class updated to include a weight integer.

--- WorkoutWhiz/src/com/mcindoe/workoutwhiz/models/Exercise.java
+++ WorkoutWhiz/src/com/mcindoe/workoutwhiz/models/Exercise.java
@@ -5,11 +5,20 @@ import java.util.ArrayList;
 public class Exercise {
 
 	private String name;
+	private int weight;
 	private ArrayList<Integer> reps;
 	private ArrayList<Integer> lastReps;
 	
 	public Exercise(String name) {
 		this.setName(name);
+		this.setWeight(0);
+		reps = new ArrayList<Integer>();
+		lastReps = new ArrayList<Integer>();
+	}
+
+	public Exercise(String name, int weight) {
+		this.setName(name);
+		this.setWeight(weight);
 		reps = new ArrayList<Integer>();
 		lastReps = new ArrayList<Integer>();
 	}
@@ -37,5 +46,13 @@ public class Exercise {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
 	}
 }
