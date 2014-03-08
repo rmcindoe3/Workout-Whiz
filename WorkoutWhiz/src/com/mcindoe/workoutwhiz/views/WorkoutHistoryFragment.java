3edaaa403cd82b4a066040e0da42ca91c2f40880
rmcindoe3 <rmcindoe3@gatech.edu>
2014-03-08 09:19:13 -0500

    Added and implemented functionality of drop down menus in history activity.

--- WorkoutWhiz/src/com/mcindoe/workoutwhiz/views/WorkoutHistoryFragment.java
+++ WorkoutWhiz/src/com/mcindoe/workoutwhiz/views/WorkoutHistoryFragment.java
@@ -3,15 +3,18 @@ package com.mcindoe.workoutwhiz.views;
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.content.DialogInterface;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 
 import com.mcindoe.workoutwhiz.R;
 import com.mcindoe.workoutwhiz.controllers.WorkoutDataSource;
@@ -27,8 +30,9 @@ public class WorkoutHistoryFragment extends Fragment {
 		public void showWorkout(Workout workout);
 	}
 	
-	private WorkoutHistoryListener sourceActivity;
-	
+	private WorkoutHistoryListener mWorkoutHistoryListener;
+	private WorkoutViewFragment.WorkoutViewButtonListener mWorkoutViewButtonListener;
+
 	@Override
 	public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -36,11 +40,12 @@ public class WorkoutHistoryFragment extends Fragment {
         // Verify that the host activity implements the callback interface
         try {
             // Instantiate the NoticeDialogListener so we can send events to the host
-            sourceActivity = (WorkoutHistoryListener) activity;
+            mWorkoutHistoryListener = (WorkoutHistoryListener) activity;
+            mWorkoutViewButtonListener = (WorkoutViewFragment.WorkoutViewButtonListener) activity;
         } catch (ClassCastException e) {
             // The activity doesn't implement the interface, throw exception
             throw new ClassCastException(activity.toString()
-                    + " must implement WorkoutHistoryListener");
+                    + " must implement WorkoutHistoryListener and WorkoutViewButtonListener");
         }	
 	}
 
@@ -94,13 +99,75 @@ public class WorkoutHistoryFragment extends Fragment {
 				
 				WorkoutLinearLayout layout = (WorkoutLinearLayout)v.findViewById(R.id.list_item_workout_history);
 				
-				sourceActivity.showWorkout(layout.getWorkout());
+				mWorkoutHistoryListener.showWorkout(layout.getWorkout());
 			}
 			//If the view that called this is our options button...
 			else if(v.getId() == R.id.list_item_workout_options_button) {
 
-				//TODO: show the options for this list item.
+				showWorkoutOptions(v);
 			}
 		}
+	}
+	
+	/**
+	 * Shows the workout options popup menu.
+	 * @param anchorView - the view that called this popup
+	 */
+	public void showWorkoutOptions(View anchorView) {
+
+		//Grabs the parent layout of the settings button - the exercise list item linear layout
+		final WorkoutLinearLayout srcLayout = ((WorkoutLinearLayout)anchorView.getParent());
+
+		//Inflate our layout
+		View workoutOptions = ((LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.popup_workout_options, null);
+		
+		//Create our popup
+		final PopupWindow workoutPopup = new PopupWindow(workoutOptions, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		
+		//We want the popup window to be focusable
+		workoutPopup.setFocusable(true);
+		
+		//We also want the popup to be dismissed when a touch is made outside of it.
+		workoutPopup.setBackgroundDrawable(new ColorDrawable());
+		
+		Button viewButton = (Button)workoutOptions.findViewById(R.id.workout_options_view_button);
+		Button performButton = (Button)workoutOptions.findViewById(R.id.workout_options_perform_button);
+		Button favoriteButton = (Button)workoutOptions.findViewById(R.id.workout_options_favorite_button);
+		Button eraseButton = (Button)workoutOptions.findViewById(R.id.workout_options_erase_button);
+		
+		viewButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mWorkoutHistoryListener.showWorkout(srcLayout.getWorkout());
+				workoutPopup.dismiss();
+			}
+		});
+
+		performButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mWorkoutViewButtonListener.performWorkout(srcLayout.getWorkout());
+				workoutPopup.dismiss();
+			}
+		});
+
+		favoriteButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mWorkoutViewButtonListener.favoriteWorkout(srcLayout.getWorkout());
+				workoutPopup.dismiss();
+			}
+		});
+
+		eraseButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mWorkoutViewButtonListener.eraseWorkout(srcLayout.getWorkout());
+				workoutPopup.dismiss();
+			}
+		});
+
+		//Show the popup as a drop down from our anchor view
+		workoutPopup.showAsDropDown(anchorView);
 	}
 }
